d4acf06dfe279333006795b0b2db08666149d6d8
YP Zhang <ypzhang@n04.prima>
2020-05-05 20:06:39 -0400

    Add the verify code and expiry for email update. Not sure if it is the best practice. Revisit later

--- pkg/dynamodb/blacklist.go
+++ pkg/dynamodb/blacklist.go
@@ -1,6 +1,6 @@
 package dynamo
 
-var Blacklist = [1000]string{".htaccess", ".htpasswd", ".well-known", "400", "401", "403", "404", "405", "406", "407", "408", "409",
+var Blacklist = [...]string{".htaccess", ".htpasswd", ".well-known", "400", "401", "403", "404", "405", "406", "407", "408", "409",
 	"410", "411", "412", "413", "414", "415", "416", "417", "421", "422", "423", "424", "426", "428", "429", "431", "500",
 	"501", "502", "503", "504", "505", "506", "507", "508", "509", "510", "511", "_domainkey", "about", "about-us", "abuse",
 	"access", "account", "accounts", "ad", "add", "admin", "administration", "administrator", "ads", "ads.txt", "advertise",
@@ -43,7 +43,7 @@ var Blacklist = [1000]string{".htaccess", ".htpasswd", ".well-known", "400", "40
 	"webmaster", "website", "widget", "widgets", "wiki", "wpad", "write", "www", "www-data", "www1", "www2", "www3", "www4", "you", "yourname", "yourusername",
 	"zlib",
 	// companies
-	"google", "facebook", "twitter", "cloudor", "nvidia", "intel", "aliyun", "azure", "alibaba", "paypal", "stripe", "docker", "kubernetes", "tesla", "geforce"}
+	"google", "facebook", "amazon", "twitter", "cloudor", "nvidia", "intel", "aliyun", "azure", "alibaba", "paypal", "stripe", "docker", "kubernetes", "tesla", "geforce"}
 
 func NewBlackListMap() map[string]bool {
 	set := make(map[string]bool) // New empty set
